c0c009c6ecb53c78003f2fb1aea286f7e7e92234
nachool <nachma.olivia1394@gmail.com>
2026-04-14 14:19:44 +0700

    menambah user controller untuk routing

--- src/main/java/com/tugas/demo/controller/UserController.java
+++ src/main/java/com/tugas/demo/controller/UserController.java
@@ -1,4 +1,61 @@
 package com.tugas.demo.controller;
 
+
+import com.tugas.demo.model.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserController {
+    private List<User> userList = new ArrayList<>();
+
+    private final String NIM_KAMU = "20230140048";
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginSubmit(@RequestParam String username, @RequestParam String password, Model model) {
+        if ("admin".equals(username) && NIM_KAMU.equals(password)) {
+            return "redirect:/home";
+        }
+        model.addAttribute("error", "Username atau Password salah!");
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("users", userList);
+        model.addAttribute("nim", NIM_KAMU);
+        return "home";
+    }
+
+    @GetMapping("/form")
+    public String formPage(Model model) {
+        model.addAttribute("user", new User());
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String formSubmit(@ModelAttribute User user) {
+        userList.add(user);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "redirect:/login";
+    }
 }
